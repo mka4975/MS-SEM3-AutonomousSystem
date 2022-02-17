248bc3d72a46f68907960e3951f2ea5553233022
Michael KAPPAURER <kami@fhv.at>
2022-02-17 09:18:01 +0100

    final changes

--- src/turtlebot_mapping/src/python/goToPosition.py
+++ src/turtlebot_mapping/src/python/goToPosition.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# TurtleBot must have minimal.launch & amcl_demo.launch
-# running prior to starting this script
-# For simulation: launch gazebo world & amcl_demo prior to run this script
-
 from turtle import pos
 import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
@@ -18,19 +14,13 @@ class GoToPose():
 
         self.goal_sent = False
 
-        # What to do if shut down (e.g. Ctrl-C or failure)
         rospy.on_shutdown(self.shutdown)
 
-    # Tell the action client that we want to spin a thread by default
         self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         rospy.loginfo("Wait for the action server to come up")
-
-    # Allow up to 5 seconds for the action server to come up
         self.move_base.wait_for_server(rospy.Duration(5))
 
     def goto(self, pos, quat):
-
-        # Send a goal
         self.goal_sent = True
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = 'map'
@@ -38,17 +28,14 @@ class GoToPose():
         goal.target_pose.pose = Pose(Point(pos['x'], pos['y'], 0.000),
                                     Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4']))
 
-    # Start moving
         self.move_base.send_goal(goal)
 
-    # Allow TurtleBot up to 60 seconds to complete task
         success = self.move_base.wait_for_result(rospy.Duration(60))
 
         state = self.move_base.get_state()
         result = False
 
         if success and state == GoalStatus.SUCCEEDED:
-                # We made it!
             result = True
         else:
             self.move_base.cancel_goal()
@@ -64,14 +51,11 @@ class GoToPose():
 
 
 def main(positionList,target):
-    allTokenFound = False
     currentTarget = target
     try:
         positionToGo = positionList[currentTarget]
-        # rospy.init_node('nav_test', anonymous=False)
         navigator = GoToPose()
 
-        # Customize the following values so they are appropriate for your location
         position = {'x': positionToGo[0], 'y': positionToGo[1]}
         quaternion = {'r1': 0.000, 'r2': 0.000, 'r3': 0.000, 'r4' : 1.000}
 
@@ -79,20 +63,13 @@ def main(positionList,target):
         success = navigator.goto(position, quaternion)
 
         if success:
-            # pub = rospy.Publisher('sound', Sound, queue_size=10)
-            # pub.publish(2)
             rospy.loginfo("Hooray, reached the desired pose")
             if currentTarget < len(positionList):
                 currentTarget += 1
-            else:
-                allTokenFound = True
             return True
         else:
             rospy.loginfo("The base failed to reach the desired pose")
             return False
-
-        # Sleep to give the last log messages time to be sent
-        rospy.sleep(1)
 
     except rospy.ROSInterruptException:
         rospy.loginfo("Ctrl-C caught. Quitting")
